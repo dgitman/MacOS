a8d5d325271cedc24145904f21da4c1ca48fa9c8
David Gitman <david@igniterate.com>
2022-02-10 08:23:50 -0500

    Create installAppsViaHomebrew.sh

---
+++ installAppsViaHomebrew.sh
@@ -0,0 +1,5 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install watch htop wget git nmap rclone shopify-cli awscli
+
+brew install --cask discord beeper
