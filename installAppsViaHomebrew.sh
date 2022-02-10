a9d115f0e45e8b2825f51d89a6d40cdb7dbaac0a
David Gitman <david@igniterate.com>
2022-02-10 09:21:56 -0500

    Update installAppsViaHomebrew.sh

--- installAppsViaHomebrew.sh
+++ installAppsViaHomebrew.sh
@@ -2,4 +2,5 @@
 
 brew install watch htop wget git nmap rclone shopify-cli awscli
 
-brew install --cask discord beeper
+# https://formulae.brew.sh/cask/
+brew install --cask 1password 1password-cli google-chrome google-drive onedrive dropbox beeper slack discord signal telegram viber messenger whatsapp zoom fantastical cardhop kindle mimestream microsoft-word microsoft-excel microsoft-powerpoint memory timely clickup visual-studio-code transmit divvy stats cleanmymac nordvpn google-trends
