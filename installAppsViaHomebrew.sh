c3679af78f9f4edfb054ea726f64d29e7fee2a8d
David Gitman <david@igniterate.com>
2022-02-11 07:56:50 -0500

    Quick Update

--- installAppsViaHomebrew.sh
+++ installAppsViaHomebrew.sh
@@ -1,6 +1,48 @@
+#!/bin/bash
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install watch htop wget git nmap rclone shopify-cli awscli
+brew install \
+watch \
+htop \
+wget \
+git \
+nmap \
+rclone \
+shopify-cli \
+awscli
 
 # https://formulae.brew.sh/cask/
-brew install --cask 1password 1password-cli google-chrome google-drive onedrive dropbox beeper slack discord signal telegram viber messenger whatsapp zoom fantastical cardhop kindle mimestream microsoft-word microsoft-excel microsoft-powerpoint memory timely clickup visual-studio-code transmit divvy stats cleanmymac nordvpn google-trends
+brew install --cask \
+1password \
+1password-cli \
+google-chrome \
+google-drive \
+onedrive \
+dropbox \
+beeper \
+slack \
+discord \
+signal \
+telegram \
+viber \
+messenger \
+whatsapp \
+zoom \
+fantastical \
+cardhop \
+kindle \
+mimestream \
+microsoft-word \
+microsoft-excel \
+microsoft-powerpoint \
+memory \
+timely \
+clickup \
+visual-studio-code \
+transmit \
+divvy \
+stats \
+cleanmymac \
+nordvpn \
+google-trends
