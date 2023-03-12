08502be3867bd5d20458a5e27d37240e6fdf9950
David Gitman <david@igniterate.com>
2023-03-12 14:50:47 -0400

    Update installAppsViaHomebrew.sh

--- installAppsViaHomebrew.sh
+++ installAppsViaHomebrew.sh
@@ -14,9 +14,7 @@ wget \
 git \
 nmap \
 speedtest-cli \
-shellcheck \
 rclone \
-shopify-cli \
 awscli
 
 # https://formulae.brew.sh/cask/
@@ -66,6 +64,9 @@ raspberry-pi-imager \
 #https://github.com/mriscoc/Ender3V2S1/wiki/How-to-generate-a-gcode-preview
 pillow \
 
+Shopify
+brew tap shopify/shopify
+brew install shopify-cli
+
 #Cleanup?
-brew tap \
-homebrew/autoupdate 
+brew tap homebrew/autoupdate 
